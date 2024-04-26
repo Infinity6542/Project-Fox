14e4a90448937db6223af618dff08a4651917970
Infinity <isd6542@gmail.com>
2024-04-26 21:19:08 +1000

    Moved clear script to core during alpha

--- scripts/core.js
+++ scripts/core.js
@@ -1,3 +1,17 @@
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+function clear() {
+  console.log("[LOGS] [PURG] [CACHE] Clearing cache...");
+  localStorage.clear();
+  console.log("[LOGS] [PURG] [CACHE] Cache successfully cleared.");
+  console.log("[LOGS] [PURG] Clearing checkboxes...");
+  let inputs = document.querySelectorAll('input[type="checkbox"]');
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].checked = false;
+  }
+  console.log("[LOGS] [PURG] Checkboxes successfully cleared.");
+  console.log("[LOGS] [PURG] Reloading page to apply changes...");
+  location.reload();
+}
--- scripts/game.js
+++ scripts/game.js
@@ -224,20 +224,6 @@ function init(name, code, status) {
   );
 }
 
-function clear() {
-  console.log("[LOGS] [PURG] [CACHE] Clearing cache...");
-  localStorage.clear();
-  console.log("[LOGS] [PURG] [CACHE] Cache successfully cleared.");
-  console.log("[LOGS] [PURG] Clearing checkboxes...");
-  let inputs = document.querySelectorAll('input[type="checkbox"]');
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].checked = false;
-  }
-  console.log("[LOGS] [PURG] Checkboxes successfully cleared.");
-  console.log("[LOGS] [PURG] Reloading page to apply changes...");
-  location.reload();
-}
-
 function set(checkbox) {
   if (checkbox.checked === true) {
     try {
