ede33478ef80552e83d52cff0d8aff254d22027e
Infinity <isd6542@gmail.com>
2024-05-12 16:28:47 +1000

    Sync and 415/416 progression

---
+++ scripts/dashboard.js
@@ -0,0 +1,21 @@
+validateFile = () => {
+  const fi = document.getElementById("upload");
+  fileName = fi.value;
+  extension = fileName.split(".").pop();
+  if (fi == "txt") {
+    if (fi.files.length > 0) {
+        for (const i = 0; i <= fi.files.length - 1; i++) {
+          const fsize = fi.files.item(i).size;
+          const file = Math.round(fsize / 1024);
+          // The size of the file.
+          if (file >= 4096) {
+            window.location.href = "/413.html";
+          } else {
+            document.getElementById("size").innerHTML = "<b>" + file + "</b> KB";
+          }
+        }
+      }
+  } else {
+    window.location.href = "/415.html";
+  }
+};
