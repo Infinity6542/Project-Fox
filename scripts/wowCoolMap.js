391a49786b7e758bb8a8858210d5bd9c9d5d1fb6
Infinity <isd6542@gmail.com>
2024-05-18 17:00:17 +1000

    OPtimisation
    Signed-off-by: Infinity <isd6542@gmail.com>

--- scripts/wowCoolMap.js
+++ scripts/wowCoolMap.js
@@ -72,7 +72,7 @@ window.onload = () => {
   } else {
     console.log("Starting out? Thanks for playing :)");
   }
-  if (loaded == true) {
+  if (!loaded) {
     document.querySelector("p").innerHTML = "rendering";
     delay(1500).then(() => {
       let e = document.querySelector(".preloader");
